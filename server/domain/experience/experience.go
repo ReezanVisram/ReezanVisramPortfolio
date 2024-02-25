fee6e21e9e0225e3e47c6ad93cdc33dd98fc6f0f
Reezan Visram <reezan.visram@rogers.com>
2024-02-25 17:16:23 -0500

    add tools to response

--- server/domain/experience/experience.go
+++ server/domain/experience/experience.go
@@ -5,4 +5,5 @@ type Experience struct {
 	JobTitle        string   `json:"job_title" bson:"job_title"`
 	StartAndEndDate string   `json:"start_and_end_date" bson:"start_and_end_date"`
 	BulletPoints    []string `json:"bullet_points" bson:"bullet_points"`
+	Tools           []string `json:"tools" bson:"tools"`
 }
