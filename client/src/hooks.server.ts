19ac85fd9b93202d122f9b6a7f5c43f9757f32b8
Reezan Visram <reezan.visram@rogers.com>
2024-03-13 22:11:16 -0400

    fix undefined theme on initial page load

--- client/src/hooks.server.ts
+++ client/src/hooks.server.ts
@@ -4,6 +4,9 @@ export const handle: Handle = async ({ event, resolve }) => {
 	const theme = event.cookies.get('siteTheme');
 	const response = await resolve(event, {
 		transformPageChunk: ({ html }) => {
+			if (!theme) {
+				return html.replace('data-theme=""', `data-theme="light"`);
+			}
 			return html.replace('data-theme=""', `data-theme="${theme}"`);
 		}
 	});
